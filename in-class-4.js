9d9317190c7b19e1c2250da404455b262967a772
rnoor1 <149001829+rnoor1@users.noreply.github.com>
2024-09-25 17:55:38 -0400

    Update in-class-4.js

--- in-class-4.js
+++ in-class-4.js
@@ -3,11 +3,15 @@ console.log("in-class-4.js loaded!");
 const array = [1, 2, 3, 4, 5];
 
 for (let i = 0; i < array.length; i++) {
-       if ( (array[i]==2) ||(array[i]==4) ) {
-        const templateLiteral = `${i} is even!`
-        console.log(templateLiteral)}
-    else if ( (array[i]==1)||(array[i]==3)|| (array[i]==5)) { 
-        const templateLiteral = `${i} is odd!`
-        console.log(templateLiteral)
+   if ( (array[i]==1)||(array[i]==3)|| (array[i]==5)) { 
+        const templateLiteral = `${array[i]} is odd!`;
+        console.log(templateLiteral);
+       
+   } else if ( (array[i]==2) ||(array[i]==4) ) {
+
+            const templateLiteral = `${array[i]} is even!`;
+
+        console.log(templateLiteral);
+    
     }
 }

1efa22137ac9c7148d734d42c88572ee34cdf428
rnoor1 <149001829+rnoor1@users.noreply.github.com>
2024-10-26 16:23:09 -0400

    Update api.php

--- HWCISC/Homework 6/api.php
+++ HWCISC/Homework 6/api.php
@@ -1,12 +1,12 @@
 <?php
-// Another associative array
+
 $apiArray = [
     "product" => "Laptop",
     "price" => 1200,
     "brand" => "BrandX"
 ];
 
-// Echo the array as JSON
+
 echo json_encode($apiArray);
 ?>
 
